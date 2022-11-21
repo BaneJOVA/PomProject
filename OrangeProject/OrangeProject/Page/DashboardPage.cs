c70b086cb2321d2c24bb2d5fe2375fe6dc9fe89c
QA36 <QA36@DESKTOP-AESHFBQ>
2022-11-21 21:01:24 +0100

    projekat

---
+++ OrangeProject/OrangeProject/Page/DashboardPage.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrangeProject.Page
+{
+    public class DashboardPage
+    {
+        private IWebDriver driver = WebDriver.Instance;
+
+        public IWebDriver UserMenu = driver FindElement(By.Class);
+
+        public void SelectOption()
+        { 
+            SelectElement element => new SelectElement(driver.FindElement())
+
+
+
+    }
+}
